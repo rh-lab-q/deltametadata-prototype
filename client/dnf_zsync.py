e4b88be57ad02f5de6af5c92a20ec4cf5978e8cc
Pavol Vican <xvican01@stud.fit.vutbr.cz>
2016-08-24 16:29:36 +0200

    client: various fixes

--- client/dnf_zsync.py
+++ client/dnf_zsync.py
@@ -1,17 +1,20 @@
-import os.path
 import os
 import re
-from subprocess import PIPE, CalledProcessError, Popen, check_output
-
+from subprocess import PIPE, DEVNULL, CalledProcessError, Popen, check_output, check_call
 import dnf
+import logging
+
+logger = logging.getLogger("dnf")
 
 
 class PluginImpl(object):
 
-    def __init__(self, mtdt_url, print_log=False):
+    def __init__(self, mtdt_url):
         self.mtdt_url = mtdt_url
         self._cache_dir = None
-        self._print_log = print_log
+        self.wget_download = ['comps.*\.xz', 'updateinfo\.xml\.xz',
+                              'prestodelta\.xml\.xz']
+        self.zsync_download = ['primary\.xml\.gz', 'filelists\.xml\.gz']
 
     def download_repomd(self):
         " May throw if repomd.xml does not exists at server "
@@ -23,7 +26,7 @@ class PluginImpl(object):
     def load_local_repomd(self):
         " return None if cache_dir/repomd.xml was not found "
         try:
-            with open(self._cache_dir + '/repomd.xml', 'r') as repomd:
+            with open(self._cache_dir + '/repodata/repomd.xml', 'r') as repomd:
                 return repomd.read()
         except:
             pass
@@ -36,37 +39,61 @@ class PluginImpl(object):
         if repomd:
             return re.search(
                 r'<location href=\"repodata/(.*' +
-                self.repodata_base_name(file_name) +
-                r')\"',
+                file_name + r')\"',
                 repomd
             ).group(1)
         else:
             return file_name
 
-    def sync_metadata(self, cache_dir):
-        def iter_repodata(repomd):
-            return re.finditer(
-                r'<location href=\"repodata/(.*)(\.(?:gz))\"',
-                repomd
-            )
+    def download_wget_file(self, file):
+        try:
+            check_call(['wget', self.mtdt_url + file, '-O', self._cache_dir +
+                        '/repodata/' + file], stdout=DEVNULL, stderr=DEVNULL)
+        except CalledProcessError as ex:
+            logger.debug(str(ex))
 
+    def save_repomd(self, repomd):
+        with open(self._cache_dir + '/repodata/repomd.xml', 'w') as repomd_f:
+            repomd_f.write(repomd)
+
+    def remove_file_ext(self, file_name):
+        return file_name[:file_name.rfind('.')]
+
+    def sync_metadata(self, cache_dir):
         self._cache_dir = cache_dir
+        repomd = self.download_repomd()
+
         if not os.path.exists(cache_dir):
                 os.makedirs(cache_dir)
-
-        repomd = self.download_repomd()
+                os.makedirs(cache_dir + '/repodata')
+                for file in self.wget_download:
+                    self.download_wget_file(self.get_input_name(repomd, file))
+                for file in self.zsync_download:
+                    self.download_wget_file(self.get_input_name(repomd, file))
+                self.save_repomd(repomd)
+                return
         local_repomd = self.load_local_repomd()
-        for loc in iter_repodata(repomd):
-            file_name = loc.group(1)
-            input_name = self.get_input_name(
-                local_repomd, file_name + loc.group(2))
-            self._sync(
-                self.mtdt_url + file_name + '.zsync',
-                cache_dir + '/repodata/' + input_name,
-                cache_dir + '/repodata/' + file_name + loc.group(2)
-            )
-        with open(cache_dir + '/repomd.xml', 'w') as repomd_f:
-            repomd_f.write(repomd)
+        os.chdir(cache_dir + '/repodata')
+
+        for file in self.wget_download:
+            new_file = self.get_input_name(repomd, file)
+            old_file = self.get_input_name(local_repomd, file)
+            if new_file.find(old_file) != 0:
+                if (os.path.isfile(cache_dir + '/repodata/' + old_file)):
+                    os.remove(cache_dir + '/repodata/' + old_file)
+                self.download_wget_file(new_file)
+
+        for file in self.zsync_download:
+            new_file = self.get_input_name(repomd, file)
+            old_file = self.get_input_name(local_repomd, file)
+            if new_file.find(old_file) != 0:
+                self._sync(
+                    self.mtdt_url + self.remove_file_ext(new_file) + '.zsync',
+                    cache_dir + '/repodata/' + old_file,
+                    cache_dir + '/repodata/' + new_file
+                )
+
+        self.save_repomd(repomd)
 
     def _sync(self, url, input_file, target):
         " this is exception safe (unless something unexpected will happen) "
@@ -77,11 +104,11 @@ class PluginImpl(object):
             zsync = Popen(['zsync', url, '-i', input_file, '-o',
                            target], stdout=PIPE, stderr=PIPE)
             outputs = zsync.communicate()
-            if self._print_log:
-                print(outputs[1].decode('utf-8'))
-                print(outputs[0].decode('utf-8'))
+            if zsync.returncode:
+                logger.debug(outputs[1].decode('utf-8'))
+                logger.debug(outputs[0].decode('utf-8'))
         except CalledProcessError as ex:
-            # print(str(ex), file=sys.stderr)
+            logger.debug(str(ex))
             # reverse rewriting existing if there was any
             try:
                 check_output(['mv', target + '.zs-old', target])
