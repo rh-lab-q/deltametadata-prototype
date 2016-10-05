a634258ca212eaf8f0ce62d20ed4e4531d5bfe1f
Pavol Vican <xvican01@stud.fit.vutbr.cz>
2016-10-05 22:41:19 +0200

    change ci - add test for checking zsync

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 STATUS_ALL=0
-
+URL="http://dmd-deltametadata.rhcloud.com/local/"
+CACHE_DIR="/var/cache/dnf/updates-*/repodata/"
 GITROOT=$(pwd)$(git rev-parse --show-cdup)
 
 echo "$GITROOT"
@@ -18,11 +19,64 @@ fi
 
 echo "Commit range: $COMMIT_RANGE"
 
-echo "Copare output from repoquery"
+docker exec -i test_fedora bash -c "dnf repoquery -q --releasever=23 --disablerepo=\* --enablerepo=updates > /dev/null"
+((STATUS_ALL+=$?))
+echo -en "travis_fold:start:zsync\\r"
+docker exec -i test_fedora bash -c "mkdir /tmp/compare; cd /tmp/compare; STATUS_ALL=0;
+                                    echo -en \"travis_fold:start:repomd.xml\\r\"
+                                    wget $URL\"repomd.xml\" >output.out 2>&1
+                                    echo -e \"diff files\" >>output.out
+                                    diff repomd.xml ${CACHE_DIR}repomd.xml 2>>output.out
+                                    STATUS=\$?
+                                    if [ \$STATUS == 0 ] ; then
+                                        echo \"Compare repomd.xml $(tput setaf 2)succeeded $(tput sgr0)\"
+                                    else
+                                        echo \"Compare repomd.xml $(tput setaf 1)failed$(tput sgr0)\"
+                                    fi
+                                    ((STATUS_ALL+=STATUS))
+                                    cat output.out
+                                    echo -en \"travis_fold:end:repomd.xml\\r\"
+                                    diff repomd.xml ${CACHE_DIR}repomd.xml; ((STATUS+=\$?))
+                                    for i in \$(/dnf/scripts/parse_repomd.py repomd.xml)
+                                    do 
+                                        echo -en \"travis_fold:start:\${i#*-}\\r\"
+                                        wget $URL\$i >output.out 2>&1
+                                        echo -e \"diff files\" >>output.out
+                                        diff \$i ${CACHE_DIR}\$i 2>>output.out
+                                        STATUS=\$?
+                                        if [ \$STATUS == 0 ] ; then
+                                            echo \"Compare \${i#*-} $(tput setaf 2)succeeded $(tput sgr0)\"
+                                        else
+                                            echo \"Compare \${i#*-} $(tput setaf 1)failed$(tput sgr0)\"
+                                        fi
+                                        ((STATUS_ALL+=STATUS))
+                                        cat output.out
+                                        echo -en \"travis_fold:end:\${i#*-}\\r\"
+                                    done
+                                    exit \$STATUS_ALL" >output.out 2>&1
+STATUS=$?
+if [ $STATUS == 0 ] ; then
+    echo "Zsync $(tput setaf 2)succeeded $(tput sgr0)"
+else
+    echo "Zsync $(tput setaf 1)failed$(tput sgr0)"
+fi
+cat output.out
+((STATUS_ALL+=$STATUS))
+echo -en "travis_fold:end:zsync\\r"
 
-docker exec -i test_fedora bash -c "dnf repoquery -q --releasever=23 --disablerepo=\* --enablerepo=updates > /dnf/deltametadata_pkgs.txt"
-docker exec -i test_fedora bash -c "dnf repoquery -q --disableplugin zsync --disablerepo=\* --enablerepo=updates --releasever=23 > /dnf/updates_pkgs.txt"
-diff /dnf/deltametadata_pkgs.txt /dnf/updates_pkgs.txt
-STATUS_ALL=$?
+echo -en "travis_fold:start:repoquery\\r"
+docker exec -i test_fedora bash -c "dnf repoquery -q --releasever=23 --disablerepo=\* --enablerepo=updates > /dnf/deltametadata_pkgs.txt
+                                    dnf repoquery -q --disableplugin dnf_zsync --disablerepo=\* --enablerepo=updates --releasever=23 > /dnf/updates_pkgs.txt
+                                    diff /dnf/deltametadata_pkgs.txt /dnf/updates_pkgs.txt
+                                    exit \$?" >output.out 2>&1
+STATUS=$?
+if [ $STATUS == 0 ] ; then
+    echo "Copare output from repoquery $(tput setaf 2)succeeded $(tput sgr0)"
+else
+    echo "Copare output from repoquery $(tput setaf 1)failed$(tput sgr0)"
+fi
+cat output.out
+((STATUS_ALL+=$STATUS))
+echo -en "travis_fold:end:repoquery\\r"
 
 exit $STATUS_ALL
